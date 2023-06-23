5acffd540202f2e9fa286f1cf89dd764aa291537
andywhite1981 <andrey.travenko@gmail.com>
2023-06-23 15:19:42 +0300

    express middlewares

---
+++ 12-express/04-middlewares/app.mjs
@@ -0,0 +1,18 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+// logs info about request
+app.use(morgan('tiny'));
+// converts JSON to JS Object in POST, PUT, PATCH requests
+app.use(express.json());
+// converts form data to JS Object in POST, PUT, PATCH requests
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res) => {
+    console.log(req.body);
+    res.send('This is express server');
+});
+
+app.listen(5000, () => console.log('server is listening on port 5000'));
